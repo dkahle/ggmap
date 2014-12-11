2d0303d2b71928d849740ddd5b50d13e219e3732
hadley <h.wickham@gmail.com>
2014-12-11 08:57:33 -0600

    Use slippy map code for stamen

--- R/get_stamenmap.R
+++ R/get_stamenmap.R
@@ -103,302 +103,47 @@ get_stamenmap <- function(
   crop = TRUE, messaging = FALSE, urlonly = FALSE, color = c("color","bw"), force = FALSE,
   where = tempdir(), ...
 ){
-
-  # enumerate argument checking (added in lieu of checkargs function)
-  args <- as.list(match.call(expand.dots = TRUE)[-1])
-  argsgiven <- names(args)
-
-  if("bbox" %in% argsgiven){
-    if(!(is.numeric(bbox) && length(bbox) == 4)){
-      stop("bounding box improperly specified.  see ?get_openstreetmap", call. = F)
-    }
-  }
-
-  if("zoom" %in% argsgiven){
-    if(!(is.numeric(zoom) && length(zoom) == 1 &&
-    zoom == round(zoom) && zoom >= 0 && zoom <= 18)){
-      stop("scale must be a postive integer 0-18, see ?get_stamenmap.", call. = F)
-    }
-  }
-
-  if("messaging" %in% argsgiven) stopifnot(is.logical(messaging))
-
-  if("urlonly" %in% argsgiven) stopifnot(is.logical(urlonly))
-
-
-  # color arg checked by match.arg
-
-  if("checkargs" %in% argsgiven){
-    .Deprecated(msg = "checkargs argument deprecated, args are always checked after v2.1.")
-  }
-
-  # argument checking (no checks for language, region, markers, path, visible, style)
-  #args <- as.list(match.call(expand.dots = TRUE)[-1])
-  #if(checkargs) get_stamenmap_checkargs(args)
   maptype <- match.arg(maptype)
-  color <- match.arg(color)
-  if(is.null(names(bbox))) names(bbox) <- c("left","bottom","right","top")
 
-  # set image type (stamen only)
-  if(maptype %in% c("terrain","terrain-background","watercolor")){
-    filetype <- "jpg"
-  } else {
-    filetype <- "png"
+  bbox <- check_bbox(bbox)
+  zoom <- check_zoom(zoom)
+
+  # Remove in 2.5
+  if (!missing(crop)) {
+    warning("crop argument is deprecated", call. = FALSE)
+  }
+  if (!missing(urlonly)) {
+    warning("urlonly argument is deprecated", call. = FALSE)
+  }
+  if (!missing(color)) {
+    warning("color argument is deprecated", call. = FALSE)
   }
 
-  # determine tiles to get
-  fourCorners <- expand.grid(
-    lon = c(bbox["left"], bbox["right"]),
-    lat = c(bbox["bottom"], bbox["top"])
-  )
-  fourCorners$zoom <- zoom
-  row.names(fourCorners) <- c("lowerleft","lowerright","upperleft","upperright")
-  fourCornersTiles <- apply(fourCorners, 1, function(v) LonLat2XY(v[1],v[2],v[3]))
+  fetch_region(bbox[c("left", "right")], bbox[c("bottom", "top")], zoom = zoom,
+    provider = "stamen", style = maptype, cache = !force)
 
-  xsNeeded <- Reduce(":", sort(unique(as.numeric(sapply(fourCornersTiles, function(df) df$X)))))
-  numXTiles <- length(xsNeeded)
-  ysNeeded <- Reduce(":", sort(unique(as.numeric(sapply(fourCornersTiles, function(df) df$Y)))))
-  numYTiles <- length(ysNeeded)
-  tilesNeeded <- expand.grid(x = xsNeeded, y = ysNeeded)
-  if(nrow(tilesNeeded) > 40){
-    message(paste0(nrow(tilesNeeded), " tiles needed, this may take a while ",
-      "(try a smaller zoom)."))
-  }
-
-
-  # make urls - e.g. http://tile.stamen.com/[maptype]/[zoom]/[x]/[y].jpg
-  base_url <- "http://tile.stamen.com/"
-  base_url <- paste(base_url, maptype, "/", zoom, sep = "")
-  urls <- paste(base_url,
-    apply(tilesNeeded, 1, paste, collapse = "/"), sep = "/")
-  urls <- paste(urls, filetype, sep = ".")
-  if(messaging) message(length(urls), " tiles required.")
-  if(urlonly) return(urls)
-
-
-  # make list of tiles
-  count <- 0
-  nTiles <- nrow(tilesNeeded)
-  listOfTiles <- lapply(split(tilesNeeded, 1:nrow(tilesNeeded)), function(v){
-    v <- as.numeric(v)
-    get_stamenmap_tile(maptype, zoom, v[1], v[2], force = force, messaging = messaging)
-  })
-
-
-  # stitch tiles together
-  map <- stitch(listOfTiles)
-
-
-  # format map and return if not cropping
-  if(!crop) return(map)
-
-
-  # crop map
-  if(crop){
-  	mbbox <- attr(map, "bb")
-
-  	size <- 256 * c(length(xsNeeded), length(ysNeeded))
-    slon <- seq(mbbox$ll.lon, mbbox$ur.lon, length.out = size[1])
-    slat <- seq(mbbox$ll.lat, mbbox$ur.lat, length.out = size[2])
-
-    keep_x_ndcs <- which(bbox["left"] <= slon & slon <= bbox["right"])
-    keep_y_ndcs <- sort( size[2] - which(bbox["bottom"] <= slat & slat <= bbox["top"]) )
-
-    croppedmap <- map[keep_y_ndcs, keep_x_ndcs]
-  }
-
-
-  # format map
-  croppedmap <- as.raster(croppedmap)
-  class(croppedmap) <- c("ggmap","raster")
-  attr(croppedmap, "bb") <- data.frame(
-    ll.lat = bbox["bottom"], ll.lon = bbox["left"],
-    ur.lat = bbox["top"], ur.lon = bbox["right"]
-  )
-
-
-  # return
-  croppedmap
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-get_stamenmap_checkargs <- function(args){
-  eargs <- lapply(args, eval)
-  argsgiven <- names(args)
-
-  with(eargs,{
-
-    # bbox arg
-    if("bbox" %in% argsgiven){
-      if(!(is.numeric(bbox) && length(bbox) == 4)){
-        stop("bounding box improperly specified.  see ?get_openstreetmap", call. = F)
-      }
-    }
-
-    # zoom arg
-    if("zoom" %in% argsgiven){
-      if(!(is.numeric(zoom) && length(zoom) == 1 &&
-      zoom == round(zoom) && zoom >= 0 && zoom <= 18)){
-        stop("scale must be a postive integer 0-18, see ?get_stamenmap.", call. = F)
-      }
-    }
-
-    # messaging arg
-    if("messaging" %in% argsgiven){
-      stopifnot(is.logical(messaging))
-    }
-
-    # urlonly arg
-    if("urlonly" %in% argsgiven){
-      stopifnot(is.logical(urlonly))
-    }
-
-
-    # color arg checked by match.arg
-
-
-  }) # end with
+check_bbox <- function(x) {
+  if (!is.numeric(x) || length(x) != 4) {
+    stop("bounding box improperly specified.", call. = FALSE)
+  }
+  if (is.null(names(x))) {
+    names(x) <- c("left","bottom","right","top")
+  }
+  x
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-get_stamenmap_tile <- function(maptype, zoom, x, y, force = FALSE, messaging = TRUE, where = tempdir()){
-
-  # check arguments
-  is.wholenumber <-
-    function (x, tol = .Machine$double.eps^0.5) abs(x - round(x)) < tol
-
-  stopifnot(is.wholenumber(zoom) || !(zoom %in% 1:20))
-  stopifnot(is.wholenumber(x) || !(0 <= x && x < 2^zoom))
-  stopifnot(is.wholenumber(y) || !(0 <= y && y < 2^zoom))
-
-  # format url http://tile.stamen.com/[maptype]/[zoom]/[x]/[y].jpg
-  if(maptype %in% c("terrain","terrain-background","watercolor")){
-    filetype <- "jpg"
-  } else {
-    filetype <- "png"
+check_zoom <- function(x) {
+  if (!is.numeric(x) || length(x) != 1) {
+    stop("zoom must be a numeric vector of length 1", call. = FALSE)
   }
-  url <- paste0(paste0(c("http://tile.stamen.com", maptype, zoom, x, y), collapse = "/"), ".", filetype)
 
-  # lookup in archive
-  tile <- file_drawer_get(url)
-  if (!is.null(tile) && !force) return(tile)
+  x <- round(x)
 
-  # grab if not in archive
-  tmp <- tempfile()
-  download.file(url, destfile = tmp, quiet = !messaging, mode = "wb")
-  if(TRUE) message(paste0("Map from URL : ", url))
-
-  # read in and format
-  if(maptype %in% c("terrain","terrain-background","watercolor")){
-    tile <- readJPEG(tmp)
-  } else {
-    tile <- readPNG(tmp)
+  if (x < 0 | x > 18) {
+    stop("zoomm must lie in 0-18", call. = FALSE)
   }
-  tile <- t(apply(tile, 2, rgb))
 
-  # determine bbox of map. note : not the same as the argument bounding box -
-  # the map is only a covering of the bounding box extent the idea is to get
-  # the lower left tile and the upper right tile and compute their bounding boxes
-  # tiles are referenced by top left of tile, starting at 0,0
-  # see http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
-  lonlat_upperleft <- XY2LonLat(x, y, zoom)
-  lonlat_lowerright <- XY2LonLat(x, y, zoom, 255, 255)
-  bbox <- c(
-    left = lonlat_upperleft$lon,
-    bottom = lonlat_lowerright$lat,
-    right = lonlat_lowerright$lon,
-    top = lonlat_upperleft$lat
-  )
-  bb <- data.frame(
-    ll.lat = unname(bbox["bottom"]),
-    ll.lon = unname(bbox["left"]),
-    ur.lat = unname(bbox["top"]),
-    ur.lon = unname(bbox["right"])
-  )
-
-  # format
-  class(tile) <- c("ggmap", "raster")
-  attr(tile, "bb") <- bb
-
-  file_drawer_set(url, tile)
-  tile
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-stitch <- function(tiles){
-
-  # trick R CMD check
-  ll.lat <- NULL; rm(ll.lat);
-  ll.lon <- NULL; rm(ll.lon);
-
-  # determine bounding box
-  bbs <- ldply(tiles, function(x) attr(x, "bb"))
-
-  bigbb <- data.frame(
-    ll.lat = min(bbs$ll.lat),
-    ll.lon = min(bbs$ll.lon),
-    ur.lat = max(bbs$ur.lat),
-    ur.lon = max(bbs$ur.lon)
-  )
-
-  # determine positions of tile in slate (aggregate)
-  order <- as.numeric( arrange(bbs, desc(ll.lat), ll.lon)$.id )
-  tiles <- tiles[order]
-  tiles <- lapply(tiles, as.matrix) # essential for cbind/rbind to work properly!
-
-  # split tiles, then squeeze together from top and bottom
-  # and then squeeze together from left and right
-  nrows <- length( unique(bbs$ll.lat) )
-  ncols <- length( unique(bbs$ll.lon) )
-  tiles <- split(tiles, rep(1:nrows, each = ncols))
-  tiles <- lapply(tiles, function(x) Reduce(cbind, x))
-  tiles <- Reduce(rbind, tiles)
-
-  tiles <- as.raster(tiles)
-  class(tiles) <- c("ggmap", "raster")
-  attr(tiles, "bb") <- bigbb
-
-  tiles
+  x
 }
