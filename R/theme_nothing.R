2bf4c7fb1012ef3e16df5f424e174d0b7c7ce978
David Kahle <david.kahle@gmail.com>
2015-12-04 23:50:32 -0600

    update theme_nothing to ggplot 1.0.2

--- R/theme_nothing.R
+++ R/theme_nothing.R
@@ -1,14 +1,14 @@
 #' Make a blank ggplot2 theme.
 #'
 #' theme_nothing simply strips all thematic element in ggplot2.
-#' 
+#'
 #' @param base_size base size, not used.
 #' @param legend should the legend be included?
 #' @return a ggplot2 theme (i.e., a list of class options).
 #' @author David Kahle \email{david.kahle@@gmail.com}
 #' @export
 #' @examples
-#' library(ggplot2)
+
 #' \dontrun{
 #'
 #' n <- 50
@@ -17,23 +17,21 @@
 #' p
 #' p + theme_nothing()
 #' p + theme_nothing(legend = TRUE) # no difference
-#' p + 
+#' p +
 #'   scale_x_continuous(expand = c(0,0)) +
 #'   scale_y_continuous(expand = c(0,0)) +
 #'   theme_nothing()
-#' 
-#' qplot(1:10,1:10) +
-#'   theme_nothing()  +
-#'   theme(panel.background = element_rect(fill = "black"))
 #'
-#' 
+#'
+#'
+#'
 #' df$class <- factor(sample(0:1, .5*n^2,  replace = TRUE))
 #' p <- qplot(x, y, data = df, geom = "tile", fill = class)
 #' p
 #' p + theme_nothing()
 #' p + theme_nothing(legend = TRUE)
-#' 
-#' p <- p + 
+#'
+#' p <- p +
 #'   scale_x_continuous(expand = c(0,0)) +
 #'   scale_y_continuous(expand = c(0,0))
 #' p
@@ -49,23 +47,21 @@ theme_nothing <- function(base_size = 12, legend = FALSE){
      axis.title =         element_blank(),
      panel.background =   element_blank(),
      panel.grid.major =   element_blank(),
-     panel.grid.minor =   element_blank(),     
+     panel.grid.minor =   element_blank(),
      axis.ticks.length =  unit(0, "cm"),
-     axis.ticks.margin =  unit(0.01, "cm"), # change back to 0 when grid is fixed
      panel.margin =       unit(0, "lines"),
-     plot.margin =        unit(c(0, 0, -.5, -.5), "lines"),
+     plot.margin =        unit(c(0, 0, 0, 0), "lines"),
      complete = TRUE
-   ))    
+   ))
   } else {
    return(theme(
      line =               element_blank(),
      rect =               element_blank(),
      text =               element_blank(),
      axis.ticks.length =  unit(0, "cm"),
-     axis.ticks.margin =  unit(0.01, "cm"), # change back to 0 when grid is fixed
      legend.position =    "none",
      panel.margin =       unit(0, "lines"),
-     plot.margin =        unit(c(0, 0, -.5, -.5), "lines"),
+     plot.margin =        unit(c(0, 0, 0, 0), "lines"),
      complete = TRUE
    ))
  }
