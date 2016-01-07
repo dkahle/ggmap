2a3eb535f616cb98b13fdd0ba5040686457b69f8
David Kahle <david.kahle@gmail.com>
2016-01-07 08:53:01 -0600

    Closes #81

--- R/ggplot2.R
+++ R/ggplot2.R
@@ -4,7 +4,7 @@
 # Stat        <- get("Stat",        envir = asNamespace("ggplot2"))
 is.constant <- get("is.constant", envir = asNamespace("ggplot2"))
 rename_aes  <- get("rename_aes",  envir = asNamespace("ggplot2"))
-# .all_aesthetics <- get(".all_aesthetics", envir = asNamespace("ggplot2"))
+.all_aesthetics <- get(".all_aesthetics", envir = asNamespace("ggplot2"))
 
 list_to_dataframe <- get("list_to_dataframe", envir = asNamespace("plyr"))
 
