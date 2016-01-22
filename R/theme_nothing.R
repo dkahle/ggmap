14880dd6a2f57619407a9234a842e2c35dca1562
David Kahle <david.kahle@gmail.com>
2016-01-22 08:24:26 -0600

    theme_nothing: update examples

--- R/theme_nothing.R
+++ R/theme_nothing.R
@@ -8,9 +8,9 @@
 #' @author David Kahle \email{david.kahle@@gmail.com}
 #' @export
 #' @examples
-
-#' \dontrun{
 #'
+#'
+#' # no legend example
 #' n <- 50
 #' df <- expand.grid(x = 1:n,y = 1:n)[sample(n^2,.5*n^2),]
 #' p <- qplot(x, y, data = df, geom = 'tile')
@@ -24,7 +24,7 @@
 #'
 #'
 #'
-#'
+#' # legend example
 #' df$class <- factor(sample(0:1, .5*n^2,  replace = TRUE))
 #' p <- qplot(x, y, data = df, geom = "tile", fill = class)
 #' p
@@ -38,7 +38,7 @@
 #' p + theme_nothing()
 #' p + theme_nothing(legend = TRUE)
 #'
-#' }
+#'
 #'
 theme_nothing <- function(base_size = 12, legend = FALSE){
   if(legend){
