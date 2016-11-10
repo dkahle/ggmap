2b49464c407256b0e3c0d5942e3049992ddea540
Mikko Korpela <mvkorpel@iki.fi>
2016-11-10 17:49:42 +0200

    Make example code more usable when called with 'example(gglocator)'

--- R/gglocator.R
+++ R/gglocator.R
@@ -20,16 +20,22 @@
 #'
 #' # only run for interactive sessions
 #' df <- expand.grid(x = 0:-5, y = 0:-5)
-#' (p <- qplot(x, y, data = df) +
-#'   annotate(geom = "point", x = -2, y = -2, colour = "red"))
-#' (pt <- gglocator()) # click red point
-#' last_plot() +
+#' p <- qplot(x, y, data = df) +
+#'   annotate(geom = "point", x = -2, y = -2, colour = "red")
+#' print(p)
+#' cat("click red point\n")
+#' print(pt <- gglocator())
+#' p2 <- last_plot() +
 #'   annotate("point", pt$x, pt$y, color = "blue", size = 3, alpha = .5)
+#' cat("a blue point should appear where you clicked\n")
+#' print(p2)
 #'
-#' p +
+#' p3 <- p +
 #'   scale_x_continuous(expand = c(0,0)) +
 #'   scale_y_continuous(expand = c(0,0))
-#' gglocator(1, xexpand = c(0,0), yexpand = c(0,0))
+#' print(p3)
+#' cat("click any point\n")
+#' print(gglocator(1, xexpand = c(0,0), yexpand = c(0,0)))
 #'
 #'
 #' }
