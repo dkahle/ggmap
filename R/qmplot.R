915a93ee25e5cafddb782bf72dc80c428bf34263
Mikko Korpela <mvkorpel@iki.fi>
2016-11-24 13:38:50 +0200

    Move case study with undeclared dependencies back to \dontrun

--- R/qmplot.R
+++ R/qmplot.R
@@ -117,9 +117,9 @@
 #'   scale_alpha("Wind Speed\nand\nDirection", range = c(.1, .75)) +
 #'   guides(fill = guide_legend(), alpha = guide_legend())
 #'
+#' } # end donttest
 #'
-#'
-#'
+#' \dontrun{
 #' ## kriging
 #' ############################################################
 #' # the below examples show kriging based on undeclared packages
@@ -128,9 +128,7 @@
 #' # they also require the rgdal library
 #'
 #'
-#' library(lattice)
-#' library(sp)
-#' library(rgdal)
+#' if (require("lattice") && require("sp") && require("rgdal")) {
 #'
 #' # load in and format the meuse dataset (see bivand, pebesma, and gomez-rubio)
 #' data(meuse)
@@ -159,10 +157,8 @@
 #'
 #'
 #'
-#'
-#'
+#' if (require("gstat")) { # still requiring lattice, sp and rgdal
 #' # load in the meuse.grid dataset (looking toward kriging)
-#' library(gstat)
 #' data(meuse.grid)
 #' coordinates(meuse.grid) <- c("x", "y")
 #' proj4string(meuse.grid) <- CRS("+init=epsg:28992")
@@ -252,13 +248,9 @@
 #'   ) +
 #'   scale +
 #'   scale_size("Observed\nLog Zinc")
+#' }} # end two cases of if (require ...)
 #'
-#'
-#'
-#'
-#'
-#'
-#' } # end donttest
+#' } # end dontrun
 #'
 qmplot <- function(x, y, ..., data, zoom, source = "stamen", maptype = "toner-lite",
   extent = "device", legend = "right", padding = .02, force = FALSE,
