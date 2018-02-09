15baa9cfcbab4183d6bd0accab1d0670e77ce1cc
David Kahle <david.kahle@gmail.com>
2018-02-08 20:50:50 -0600

    rename help.R -> ggmap-package.R

--- R/ggmap-package.R
+++ R/ggmap-package.R
@@ -1,11 +1,12 @@
-#' @import ggplot2 proto RgoogleMaps png plyr reshape2 rjson mapproj
-#'   jpeg geosphere bitops
+#' @import ggplot2 proto RgoogleMaps png plyr reshape2 rjson mapproj jpeg
+#'   geosphere bitops
 #' @docType package
 #' @name ggmap
 #' @importFrom grDevices as.raster extendrange gray rgb
 #' @importFrom stats time asOneSidedFormula
 #' @importFrom utils URLencode download.file tail
-#' @importFrom grid rasterGrob seekViewport grid.locator upViewport downViewport current.vpTree current.vpPath
+#' @importFrom grid rasterGrob seekViewport grid.locator upViewport downViewport
+#'   current.vpTree current.vpPath
 #' @importFrom scales expand_range
 #' @importFrom dplyr bind_cols filter bind_rows
 #' @aliases ggmap package-ggmap
