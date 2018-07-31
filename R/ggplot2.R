aa8d4f05db9d9d29ccba081c482b31c0a4dbe064
Richard Beare <Richard.Beare@gmail.com>
2018-07-31 13:30:04 +1000

    Hack to deal with .all_aesthetics error

--- R/ggplot2.R
+++ R/ggplot2.R
@@ -4,7 +4,7 @@
 # Stat        <- get("Stat",        envir = asNamespace("ggplot2"))
 is.constant <- get("is.constant", envir = asNamespace("ggplot2"))
 rename_aes  <- get("rename_aes",  envir = asNamespace("ggplot2"))
-.all_aesthetics <- get(".all_aesthetics", envir = asNamespace("ggplot2"))
+.all_aesthetics <- ggplot2:::ggplot_global$all_aesthetics
 
 list_to_dataframe <- get("list_to_dataframe", envir = asNamespace("plyr"))
 
