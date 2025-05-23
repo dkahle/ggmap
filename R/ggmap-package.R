a871b8b48ae896a06d1f281b07ca7fbaae0e76a1
Michael Chirico <chiricom@google.com>
2025-05-23 16:19:47 -0700

    also in roxygen

--- R/ggmap-package.R
+++ R/ggmap-package.R
@@ -3,7 +3,7 @@
 #' @importFrom png readPNG
 #' @importFrom jpeg readJPEG
 #' @importFrom plyr arrange compact ddply desc dlply is.discrete is.formula join
-#'   ldply llply .
+#'   ldply .
 #' @importFrom bitops bitOr bitShiftL bitShiftR bitAnd
 #' @importFrom grDevices as.raster extendrange gray rgb
 #' @importFrom stats time asOneSidedFormula
